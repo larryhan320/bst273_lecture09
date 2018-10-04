8ef67ad374a75abd84808bdc76a7d2264fad193d
Larry Han <larryhan@fas.harvard.edu>
2018-10-04 12:17:10 -0400

    set file path

--- word_count.py
+++ word_count.py
@@ -5,15 +5,13 @@ import argparse
 parser = argparse.ArgumentParser( description="" )
 
 parser.add_argument(
-	"arg",
-	help="what does this do?",
+	"data_file",
+	help="path to the file we want to read",
 )
 
-#-------------------------------------------------------------------------------
-# Are there other arguments we need?
-#-------------------------------------------------------------------------------
-
 args = parser.parse_args( )
+print(args)
+print(args.data_file) # Namespace is a type of class that uses .
 
 #-------------------------------------------------------------------------------
 # our code for analyzing the data

6a8bacefd451b1d3580f7f71eefdcd376d51544f
Larry Han <larryhan@fas.harvard.edu>
2018-10-04 12:28:11 -0400

    open and read lines of file

--- word_count.py
+++ word_count.py
@@ -10,8 +10,19 @@ parser.add_argument(
 )
 
 args = parser.parse_args( )
-print(args)
-print(args.data_file) # Namespace is a type of class that uses .
+print("args =", args)
+print("args.data_file=", args.data_file) # Namespace is a type of class that uses .
+
+fh = open(args.data_file) # Open the file called data_file using args and assign it
+print("the file handle is", fh)
+
+lines = 0
+words = 0
+chars = 0
+
+for line in fh:
+	print(line)
+
 
 #-------------------------------------------------------------------------------
 # our code for analyzing the data
